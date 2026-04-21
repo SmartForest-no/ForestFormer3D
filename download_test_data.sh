6ce03e37725353c58cfae78046ad1f347638986c
Damien Gilliard <damien.gilliard@epfl.ch>
2026-04-21 14:48:41 +0200

    feat: add download of train data

--- download_test_data.sh
+++ download_test_data.sh
@@ -8,7 +8,12 @@ cd "$(dirname "$0")"
 
 mkdir -p data/ForAINetV2/test_data
 wget "https://zenodo.org/records/16742708/files/test_data.zip?download=1" -O test_data.zip
-unzip test_data.zip -d data/ForAINetV2/test_data
+unzip test_data.zip -d data/ForAINetV2
 rm test_data.zip
+
+mkdir -p data/ForAINetV2/train_val_data
+wget "https://zenodo.org/records/16742708/files/train_val_data.zip?download=1" -O train_val_data.zip
+unzip train_val_data.zip -d data/ForAINetV2
+rm train_val_data.zip
 
 echo "Test data downloaded and extracted to data/ForAINetV2/test_data"
